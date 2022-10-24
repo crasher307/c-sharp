1d68439be3fb90879f3e5efb6ee6b50d2d6b9da6
crasher307 <crasher307@gmail.com>
2022-10-24 15:12:25 +0500

    upd pr2

--- lesson2/homework/pr2/Program.cs
+++ lesson2/homework/pr2/Program.cs
@@ -1,19 +1,28 @@
-﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+﻿using func;
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-bool getStr(string message, out string? variable) {
-	Console.Write($"{message}: ");
-	string str = Console.ReadLine();
-	bool result = int.TryParse(str, out int num);
-	variable = result ? $"{num}" : null;
-	return result;
-}
+// -------------------------------------------------------------------
 
-string? number = null;
-while (!getStr("Введите число", out number)) {
-	Console.WriteLine("-- Попробуйте снова --");
-}
+// bool getStr(string message, out string? variable) {
+// 	Console.Write($"{message}: ");
+// 	string str = Console.ReadLine();
+// 	bool result = int.TryParse(str, out int num);
+// 	variable = result ? $"{num}" : null;
+// 	return result;
+// }
 
-Console.WriteLine($"number - {number}; result - " + (number.Length > 2 ? $"{number[2]}" : "третьей цифры нет"));
+// string? number = null;
+// while (!getStr("Введите число", out number)) {
+// 	Console.WriteLine("-- Попробуйте снова --");
+// }
+
+// Console.WriteLine($"number - {number}; result - " + (number.Length > 2 ? $"{number[2]}" : "третьей цифры нет"));
+
+string num = rw.getInt().ToString();
+Console.WriteLine($"{num}; 3 цифра - " + (num.Length > 2 ? $"{num[2]}" : "третьей цифры нет"));
+
+// -------------------------------------------------------------------
