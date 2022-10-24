d2d75cf519f0be69ca00b3f3d128187ed84bde15
crasher307 <crasher307@gmail.com>
2022-10-24 17:20:17 +0500

    add lesson3

---
+++ lesson3/work/pr1/Program.cs
@@ -0,0 +1,39 @@
+﻿using func;
+
+// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+//   2  |  1
+// -----------
+//   3  |  4
+
+// -------------------------------------------------------------------
+
+(int x, int y) getCoord() {
+	int x = 0;
+	int y = 0;
+	while (x == 0 || y == 0) {
+		if (x == 0) x = rw.getInt("Введите координаты X");
+		if (y == 0) y = rw.getInt("Введите координаты Y");
+	}
+	return (x, y);
+}
+int getQtr(int x, int y) {
+	if (x == 0 && y == 0) return 0;
+	switch (x > 0, y > 0) {
+		case (true, true): return 1;
+		case (false, true): return 2;
+		case (false, false): return 3;
+		case (true, false): return 4;
+	}
+}
+
+var (x, y) = getCoord();
+int qtr = getQtr(x, y);
+
+rw.echo($"Точка с координатами ({x}, {y}) находится в {qtr} четверти");
+
+// -------------------------------------------------------------------
+
+// 2 типа данных:
+// 1. ссылочный (resolve) - typeof (class, list, array, char ...)
+// 2. значимый (value) - (int, double, datetime, struct, enum, long, bool, byte)
---
+++ lesson3/work/pr3/Program.cs
@@ -0,0 +1,30 @@
+﻿using func;
+
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+// -------------------------------------------------------------------
+
+(int x, int y) getCoord(string numPoint) {
+	int x = 0;
+	int y = 0;
+	while (x == 0 || y == 0) {
+		if (x == 0) x = rw.getInt($"[{numPoint}] Введите координаты X");
+		if (y == 0) y = rw.getInt($"[{numPoint}] Введите координаты Y");
+	}
+	return (x, y);
+}
+
+double getDist((int x, int y) p1, (int x, int y) p2) {
+    // AB = √((xb - xa)^2 + (yb - ya)^2)
+	return Math.Sqrt(Math.Pow(p2.x - p1.x, 2) + Math.Pow(p2.y - p1.y, 2));
+}
+
+var p1 = getCoord("1-ая точка");
+var p2 = getCoord("2-ая точка");
+var dist = Math.Round(getDist(p1, p2), 2);
+
+rw.echo($"Расстояние между точками {p1} и {p2} = {dist}");
+
+// -------------------------------------------------------------------
