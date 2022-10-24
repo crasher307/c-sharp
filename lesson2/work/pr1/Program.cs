5cc6ae791d23f569a0161d90c36912f12865a875
crasher307 <crasher307@gmail.com>
2022-10-24 15:28:31 +0500

    upd w/pr1

--- lesson2/work/pr1/Program.cs
+++ lesson2/work/pr1/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿using func;
+
+/*
 Напишите программу, которая выводит
 случайное число из отрезка [10,99] и показывает
 наибольшую цифру числа.
@@ -7,16 +9,14 @@
 85 -> 8
 */
 
-Random rnd = new Random();
+// -------------------------------------------------------------------
+
+var rnd = new Random();
 int number = rnd.Next(10, 100);
 int[] digit = {number / 10, number % 10};
 
-if (digit[1] > digit[0]) {
-	(digit[0], digit[1]) = (digit[1], digit[0]);
-}
+if (digit[1] > digit[0]) (digit[0], digit[1]) = (digit[1], digit[0]);
 
-if (digit[0] == digit[1]) {
-	Console.WriteLine($"Цифры числа {number} равны");
-} else {
-	Console.WriteLine($"Наибольшая цифра числа {number} = {digit[0]}");
-}
+rw.echo(digit[0] == digit[1] ? $"Цифры числа {number} равны" : $"Наибольшая цифра числа {number} = {digit[0]}");
+
+// -------------------------------------------------------------------
