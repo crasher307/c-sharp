f67417ca9f6ebe872cbd7d065e7bce224facb62a
crasher307 <crasher307@gmail.com>
2022-10-24 17:53:26 +0500

    fix

---
+++ func/baseFunc.cs
@@ -0,0 +1,35 @@
+﻿namespace func;
+
+public class baseFunc {
+	// Считывание ввода
+	public static string get(string message = "Введите строку") {
+		echo(message, false, "get");
+		return Console.ReadLine() ?? "";
+	}
+	
+	// Вывод сообщения
+	public static void echo(string message, bool ln = true, string color = "echo") {
+		switch (color) {
+			case "get":
+				Console.ForegroundColor = ConsoleColor.Blue;
+				message = $"{message}: ";
+				break;
+			case "echo":
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				break;
+		}
+		
+		if (ln == true) Console.WriteLine(message);
+		else Console.Write(message);
+		Console.ResetColor();
+	}
+	
+	// Вывод ошибок
+	public static void error(string type, string message) {
+		Console.BackgroundColor = ConsoleColor.Red;
+		Console.Write($" {type} ");
+		Console.BackgroundColor = ConsoleColor.Gray;
+		Console.WriteLine($" {message} ");
+		Console.ResetColor();
+	}
+}
